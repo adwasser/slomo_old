1ca3dec96624f2de40cecafbb8452354ebc61baa
Asher Wasserman <asher.d.wasserman@gmail.com>
2017-04-04 15:29:54 -0700

    module level import

--- dynamo/__init__.py
+++ dynamo/__init__.py
@@ -4,6 +4,6 @@ with open(path.join(path.split(__file__)[0], "../_version.py"),
           encoding="utf-8") as f:
     __version__ = f.readline().split("=")[1].strip("\"' \n")
 
-from . import io
+from . import *
 from .run import *
 from .models import DynamicalModel
