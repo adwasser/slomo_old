c2f160690cc60db335c427a55817a5ec6093f886
Asher Wasserman <asher.d.wasserman@gmail.com>
2017-04-11 14:16:03 -0700

    updated mass plot and formatting of walker plot

--- dynamo/plot.py
+++ dynamo/plot.py
@@ -1,5 +1,5 @@
 """Nefarious plotting"""
-
+from itertools import cycle
 import dill as pickle
 import numpy as np
 import matplotlib as mpl
@@ -21,18 +21,8 @@ label_map = {"r_s": r"$r_s$", "rho_s": r"$\rho_s$", "gamma": r"$\gamma$",
              "alpha_stars": r"$\alpha_*$", "alpha_gc": r"$\alpha_\mathrm{gc}$",
              "alpha_mass": r"$\alpha_m$", "alpha_ms": r"$\alpha_\mathrm{ms}$",
              "alpha_ls": r"$\alpha_\mathrm{ls}$",
-             "alpha_sps": r"$\alpha_\mathrm{sps}$"}
+             "alpha_sp": r"$\alpha_\mathrm{sp}$", "M_bh": r"M$_\mathrm{bh}$"}
 
-# def plotstyle():
-#     mpl.rc("figure", figsize=(12, 8))
-#     mpl.rc("font", size=12, family="serif")
-#     mpl.rc("xtick", direction="in")
-#     mpl.rc("ytick", direction="in")
-#     mpl.rc("errorbar", capsize=3)
-#     mpl.rc("savefig", bbox="tight")
-#     mpl.rc("axes.formatter", limits=(-3, 3))
-#     mpl.rc("hist", bins="auto")
-# # plotstyle()
 
 def walker_plot(outfile, skip_step=100):
     """Does the walker choose the path or does the path choose the walker?"""
@@ -52,17 +42,23 @@ def walker_plot(outfile, skip_step=100):
     n = skip_step
     steps = n * np.arange(1, niterations/n + 1)
 
-    fig, axarr = plt.subplots(ndim, sharex=True, figsize=(4, 1 * ndim))    
+    ncols = int(np.sqrt(ndim))
+    nrows = int(np.ceil(ndim / ncols))
+    fig, axarr = plt.subplots(nrows, ncols, sharex="col")
+    fig.tight_layout(w_pad=1.5, h_pad=0.5)
     for i in range(ndim):
+        col = i % ncols
+        row = int(np.floor((i - col) / ncols))
         walkers = chain[:, :, i]
         for j in range(nwalkers):
             samples = chain[j, ::n, i]
-            axarr[i].plot(steps, samples, alpha=0.3)
-        axarr[i].set_ylabel(labels[i])
-        axarr[i].set_yticks([walkers.min(), (walkers.min() + walkers.max()) / 2, walkers.max()])
-        axarr[i].yaxis.set_major_formatter(mpl.ticker.FormatStrFormatter("%.2f"))
-    axarr[-1].set_xlabel('Step number')
-
+            axarr[row][col].plot(steps, samples, alpha=0.3)
+        axarr[row][col].annotate(labels[i], xy=(0.1, 0.6), xycoords="axes fraction",
+                                 bbox={"fc":"w", "ec": "k", "pad": 4.0, "alpha": 0.5})
+        axarr[row][col].set_yticks([walkers.min(), (walkers.min() + walkers.max()) / 2, walkers.max()])
+        axarr[row][col].yaxis.set_major_formatter(mpl.ticker.FormatStrFormatter("%.1f"))
+    for col in range(ncols):
+        axarr[-1][col].set_xlabel('Step number')
     return fig, axarr
 
 
@@ -88,24 +84,28 @@ def autocorr_plot(outfile, skip_step=100, **kwargs):
         acorr_times = np.zeros(ndim)        
     n = skip_step
     steps = n * np.arange(1, flatchain.shape[0]/n + 1)
-    fig, axarr = plt.subplots(ndim, sharex=True, figsize=(4, 1 * ndim))
-    label_str = r'$\sqrt{\tau_\mathrm{int} / n} = $'
+
+    ncols = int(np.sqrt(ndim))
+    nrows = int(np.ceil(ndim / ncols))
+    label_str = r'$\sqrt{\tau_\mathrm{int} / n} = $'    
+    fig, axarr = plt.subplots(nrows, ncols, sharex="col", sharey="row")
+    # fig.tight_layout()
     for i in range(ndim):
-        # rough estimate of uncertainty fraction on mean value
+        col = i % ncols
+        row = int(np.floor((i - col) / ncols))
         unc = np.sqrt(acorr_times[i] / nsamples)
-        axarr[i].plot(steps, acorr[::n, i], alpha=0.5)
-        if unc != 0:
-            axarr[i].text(0.95, 0.90, label_str + '{:.2e}'.format(unc),
-                          transform=axarr[i].transAxes, horizontalalignment='right',
-                          verticalalignment='top')
-        axarr[i].set_ylabel(labels[i])
-        axarr[i].set_yticks([0, 0.5, 1])
-    axarr[-1].set_xlabel('Iterations')
-    axarr[-1].set_xticks([steps.min(), (steps.min() + steps.max()) / 2, steps.max()])
-    axarr[-1].xaxis.set_major_formatter(mpl.ticker.FormatStrFormatter('%.0e'))
+        axarr[row][col].plot(steps, acorr[::n, i], alpha=0.5)
+        axarr[row][col].annotate(labels[i],
+                                 xy=(0.1, 0.7), xycoords="axes fraction",
+                                 bbox={"fc":"w", "ec": "k", "pad": 4.0, "alpha": 0.5})
+        axarr[row][col].annotate(label_str + '{:.1e}'.format(unc),
+                                 xy=(0.5, 0.7), xycoords="axes fraction", fontsize=10,
+                                 bbox={"fc":"w", "ec": "k", "pad": 4.0, "alpha": 0.5})
+    for col in range(ncols):
+        axarr[-1][col].set_xlabel('Iterations')
     return fig, axarr
 
-    
+
 def corner_plot(outfile, burn_fraction=0.5, **kwargs):
     """Make an enormous corner plot, rejecting the burn-in iterations at the start."""
     chain = io.read_dataset(outfile, "chain")
@@ -130,56 +130,59 @@ def corner_plot(outfile, burn_fraction=0.5, **kwargs):
     return fig
 
 
-def mass_plot(outfile, burn_fraction=0.5, rmin=0.1,
-              rmax=500, nsamples=10000, size=50,
-              dm_model=mass.M_gNFW, st_model=mass.L_sersic_s):
-    """rmax: maximum radius in kpc, nsamples: number of posterior samples"""
+def mass_plot(outfile, burn_fraction=0.5, rmin=1, rmax=1000,
+              nsamples=10000, size=50):
+    """
+    Plot mass components and total mass.
+    outfile : str, path to hdf5 file
+    burn_fraction : float in (0, 1), fraction of chain to discard
+    rmin : float, arcsec, minimum radius
+    rmax : float arcsec, maximum radius
+    nsamples : int, number of subsamples of chain
+    size : int, number of radial points on grid
+    """
     chain = io.read_dataset(outfile, "chain")
     model = io.read_model(outfile)
+    mass_model = model.mass_model
     nwalkers, niterations, ndim = chain.shape
     assert ndim == len(model.params)
     keep = round(niterations * burn_fraction)
     samples = chain[:, keep:, :].reshape((-1, ndim))
 
-    if "dist" in model.params:
-        i = list(model.params).index("dist")
-        if model.params["dist"].transform is None:
-            dist = samples[:, i]
-        else:
-            dist = model.params["dist"].transform(samples[:, i])
-    else:
-        dist = model.constants["dist"]
-    kpc_per_arcsec = np.median(dist * radians_per_arcsec)
-    
+    # construct profiles for each mass component
     idx = np.random.choice(np.arange(samples.shape[0]), nsamples)
-    radii = np.logspace(np.log10(rmin), np.log10(rmax), size)    
-    dm_profiles = np.zeros((nsamples, size))
-    st_profiles = np.zeros((nsamples, size))
+    radii = np.logspace(np.log10(rmin), np.log10(rmax), size)
+    profiles = np.zeros((len(mass_model), nsamples, size))
     for i, param_values in enumerate(samples[idx]):
         param_map = model.params.mapping(param_values)
         kwargs = {**param_map, **model.constants}
-        dm_profiles[i] = dm_model(radii, **kwargs)
-        st_profiles[i] = st_model(radii, **kwargs)
-        
-    M_tot = dm_profiles + st_profiles
-    M_dm_low, M_dm_med, M_dm_high = np.percentile(dm_profiles, [16, 50, 84], axis=0)
-    M_star_low, M_star_med, M_star_high = np.percentile(st_profiles, [16, 50, 84], axis=0)
-    M_tot_low, M_tot_med, M_tot_high = np.percentile(M_tot, [16, 50, 84], axis=0)
-    r = radii / kpc_per_arcsec
-    
+        for j, (name, mass_component) in enumerate(mass_model.items()):
+            profiles[j][i] = mass_component(radii, **kwargs)
+
+    color = cycle(["C" + str(i) for i in range(6)])
+    style = cycle(["--", "-.", ":"])
+    label_map = {"dm": r"M$_\mathrm{dm}$", "st": r"M$_*$", "bh": r"M$_\mathrm{bh}$"}
     fig, ax = plt.subplots()
-    ax.plot(r, M_star_med, 'c-.', label='Stars')
-    ax.fill_between(r, M_star_low, M_star_high, facecolor='c', alpha=0.3)
-    ax.plot(r, M_dm_med, 'm--', label='DM')
-    ax.fill_between(r, M_dm_low, M_dm_high, facecolor='m', alpha=0.3)
-    ax.plot(r, M_tot_med, 'k-', label='Total')
-    ax.fill_between(r, M_tot_low, M_tot_high, facecolor='k', alpha=0.3)
-    ax.set_xlim(np.min(r), np.max(r))
+    for i, (name, mass_component) in enumerate(mass_model.items()):    
+        M_low, M_med, M_high = np.percentile(profiles[i], [16, 50, 84], axis=0)
+        c = next(color)
+        s = next(style)
+        try:
+            label = label_map[name]
+        except KeyError:
+            label = name
+        ax.plot(radii, M_med, c + s, label=label)
+        ax.fill_between(radii, M_low, M_high, facecolor=c, alpha=0.3)
+    # total mass profile
+    M_low, M_med, M_high = np.percentile(np.sum(profiles, axis=0), [16, 50, 84], axis=0)
+    ax.plot(radii, M_med, 'k-', label=r"M$_\mathrm{tot}$")
+    ax.fill_between(radii, M_low, M_high, facecolor='k', alpha=0.3)
+
+    ax.legend(loc="best")
+    ax.set_xlim(radii.min(), radii.max())
     ax.set_xscale('log')
     ax.set_yscale('log')
-    ax.set_xlabel('Radius  [arcsec]')
-    ax.set_ylabel(r'Enclosed mass [M$_\odot$]')
-    ax.legend(loc='best')
-
+    ax.set_xlabel('R  [arcsec]')
+    ax.set_ylabel(r'M(<R)  [M$_\odot$]')
     return fig, ax
     
